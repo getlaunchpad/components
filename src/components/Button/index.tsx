69dc78b07143e7cf9d389e1fa76876e26a41d91b
Lucas Stettner <lebra448@gmail.com>
2019-12-28 00:39:11 -0500

    added buttons

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,70 @@
+import styled, { css } from "styled-components";
+import { theme } from "../../shared/theme";
+import { ButtonHTMLAttributes } from "react";
+
+const base = css`
+  outline: none;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 600;
+  height: 40px;
+  padding: 11px 15px;
+  box-shadow: ${theme.shadow.small};
+  transition: all 100ms ease;
+`;
+
+const hover = css`
+  box-shadow: ${theme.shadow.medium};
+  transform: translateY(-1px);
+`;
+
+const active = css`
+  box-shadow: none;
+  transform: translateY(1px);
+`;
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  outline?: boolean;
+}
+
+export const Primary = styled.button<ButtonProps>`
+  ${base}
+  color: ${({ outline }) => (outline ? theme.primary.six : "#fff")};
+  border: ${({ outline }) =>
+    outline ? `1px solid ${theme.brand.default}` : "1px solid transparent"};
+  background: ${({ outline }) => (outline ? "none" : theme.brand.default)};
+
+  :hover {
+    ${hover}
+    background: ${({ outline }) =>
+      outline ? theme.primary.one : theme.primary.ten}; 
+  }
+
+  :active {
+    ${active}
+    background: ${({ outline }) =>
+      outline ? theme.primary.two : theme.primary.nine};
+  }
+`;
+
+export const Secondary = styled.button<ButtonProps>`
+  ${base}
+  color: ${({ outline }) => (outline ? theme.neutral.nine : "#fff")};
+  border: ${({ outline }) =>
+    outline ? `1px solid ${theme.neutral.eight}` : "1px solid transparent"};
+  background: ${({ outline }) => (outline ? "none" : theme.neutral.nine)};
+
+  :hover {
+    ${hover}
+    background: ${({ outline }) =>
+      outline ? theme.neutral.two : theme.neutral.eight}; 
+  }
+
+  :active {
+    ${active}
+    background: ${({ outline }) =>
+      outline ? theme.neutral.three : theme.neutral.ten};
+  }
+`;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from "./components/BaseStyles";
+export * from "./components/Button";
